2e3a53ad6de33c4e8a1862aee2bf4f8272fc4271
Shourya Srivastava <shourya2346@gmail.com>
2026-04-23 19:59:37 -0700

    fix: add Vercel domain to CORS allowed origins

--- backend/dailybits/settings.py
+++ backend/dailybits/settings.py
@@ -145,6 +145,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://dailybits-web-app.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
